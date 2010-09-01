68aafeae0f9516639916b59671f70e778abe1da1
zhouer <zhouer@4836b0db-2b5f-be01-b618-bf15b2de252e>
2010-09-01 11:04:52 +0000

    * Update index.yaml  * Move shownotes.html to templates directory  * Add settings.py to get templates directory path

---
+++ settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+
+cur_path = os.path.dirname(__file__)
+
+TEMPLATE_PATH = os.path.join(cur_path, 'templates')
--- view/shownotes.py
+++ view/shownotes.py
@@ -4,14 +4,16 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
+import settings
 from model.note import Note
 
 class DisplayHandler(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            param = { 'notes' :Note.all().filter('owner =', user) }
-            path = os.path.join(os.path.dirname(__file__), 'shownotes.html')
+            query = Note.all().filter('owner =', user).order('create_time')
+            param = { 'notes': query }
+            path = os.path.join(settings.TEMPLATE_PATH, 'shownotes.html')
             out = template.render(path, param)
             self.response.out.write(out)
         else:
